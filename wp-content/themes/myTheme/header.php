48fc9ee3d09298eb0a6a49fbb827030386c1472f
Anson <ansonchan9523@gmail.com>
2018-11-14 12:29:41 +0800

    update - meta tag

--- wp-content/themes/myTheme/header.php
+++ wp-content/themes/myTheme/header.php
@@ -13,16 +13,26 @@
 	<link rel="shortcut icon" href="/images/favicon.ico">
 
 	<!-- Search Engine -->
-	<!-- <meta name="description" content="xxx"> -->
+	<!-- 
+	<meta name="description" content="xxx"> 
+	<meta name="image" content="text">
+	-->
 	<!-- Schema.org for Google -->
-	<!-- <meta itemprop="name" content="xxx">
-	<meta itemprop="description" content="xxx"> -->
+	<!-- 
+	<meta itemprop="name" content="xxx">
+	<meta itemprop="description" content="xxx"> 
+	<meta itemprop="image" content="text">
+	-->
 	<!-- Open Graph general (Facebook, Pinterest & Google+) -->
-	<!-- <meta property="og:title" content="<?php echo bloginfo('name'); ?>"/>
-	<meta property="og:site_name" content="<?php echo bloginfo('name'); ?>"/>
-	<meta property="og:description" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:type" content="website" /> -->
+	<!-- 
+	<meta name="og:url" content="<?php echo site_url('/'); ?>">
+	<meta name="og:title" content="<?php echo bloginfo('name'); ?>"/>
+	<meta name="og:site_name" content="<?php echo bloginfo('name'); ?>"/>
+	<meta name="og:description" content="xxx"/>
+	<meta name="og:image" content="xxx"/>
+	<meta name="og:locale" content="zh_HK">
+	<meta name="og:type" content="website" />
+	-->
 	
 	<?
 	wp_enqueue_style('style', get_template_directory_uri().'/css/style.min.css', array(), null, 'all');
